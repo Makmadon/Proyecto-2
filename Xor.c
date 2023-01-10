fa0e3dc59ff5f1759a169fd1afe13fe5f4233ae1
Makmadon <57114551+Makmadon@users.noreply.github.com>
2023-01-10 09:37:18 -0400

    Update Xor.c

--- Xor.c
+++ Xor.c
@@ -1,18 +1,21 @@
 #include "Xor.h"
 
+//Funcion auxiliar para hacer la operacion XOR
 Nodo *XOR(Nodo *a, Nodo *b){
     return ((Nodo*)((uintptr_t)(a) ^(uintptr_t)(b)) );
 }
-
+//Funcion auxiliar para crear lista
 Lista *crearLista(){
     return (Lista*)malloc(sizeof(Lista));
 }
-
+//Funcion para inicializar
 void inicializar(Lista *list){
+    //En caso de que no este inicializada solo se llama a crear lista
     if(list==NULL){
         list=crearLista();
         return;
     }
+    //En caso contrario se recorre la lista haciendo free a cada uno de los nodos hasta que quede vacia
     list->ultimo=NULL;
     Nodo *prev=NULL,*curr;
     while(list->primer){
@@ -22,35 +25,41 @@ void inicializar(Lista *list){
         free(curr);
     }
 }
-
+//funcion auxiliar crear nodo
 Nodo *crearNodo(int val){
     Nodo *newp=(Nodo*)malloc(sizeof(Nodo));
     newp->elemento=val;
     return newp;
 }
-
+//Funcion que verifica si la lista esta vacia, esto solo pasa si el primer y ultimo elemento son NULL
 bool esVacia(Lista *list){
     if (list->primer==NULL && list->ultimo==NULL)
         return true;
     return false;
 }
-
+//Funcion para insertar al inicio de una lista
 bool insertarPrincipio(Lista *list,int val){
+    //se crea el nuevo nodo, si hay un error en su creacion, se retorna falso
     Nodo *newp=crearNodo(val);
         if(newp==NULL)
             return false;
+    //se verifica que la lista este vacia, si es verdadero, el nuevo nodo creado sera el primero y el ultimo
     if (esVacia(list)){
         list->primer=newp;
         list->ultimo=newp;
         return true;
     }
+    /*Si no es vacia, el actual primer elemento tiene que apuntar a este, por lo que se hace Xor con el nuevo nodo
+    no se hace XOR para retirar un NULL porque XOR con NULL es el mismo elemento, 
+    el nuevo nodo solo apuntara al primero actual, y por ultimo el nuevo nodo pasa a ser el primero de la lista*/
     list->primer->AA=XOR(list->primer->AA,newp);
     newp->AA=list->primer;
     list->primer=newp;
     return true;
 }
-
+    //Funcion para insertar al final (Es muy parecido con insertar inicio)
 bool insertarFinal(Lista *list, int val){
+    //Las mismas operaciones que antes
     Nodo *newp=crearNodo(val);
     if(newp==NULL)
         return false;
@@ -59,6 +68,7 @@ bool insertarFinal(Lista *list, int val){
         list->ultimo=newp;
         return true;
     }
+    /*En vez de hacer las operaciones con el primer elemento, se hacen con el ultimo*/
     list->ultimo->AA=XOR(list->ultimo->AA,newp);
     newp->AA=list->ultimo;
     list->ultimo=newp;
